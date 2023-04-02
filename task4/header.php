1b961e83eb26a5c795b918769b77929edb20fa6f
rightguydaniel <107143522+rightguydaniel@users.noreply.github.com>
2023-04-02 14:24:15 -0400

    Create header.php

---
+++ task4/header.php
@@ -0,0 +1,10 @@
+<?php
+    /*
+    connect to database
+    username: root
+    no password
+    table name: register
+    */
+    $connect = mysqli_connect("localhost", "root", "", "register");
+    
+?>
